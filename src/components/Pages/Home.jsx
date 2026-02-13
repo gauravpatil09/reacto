491d7bdaf1fa01c2976ad16697a6896c7aae0de9
gauravpatil09 <patilgaurav9832@gmail.com>
2026-02-13 18:55:02 +0530

    ok

---
+++ src/components/Pages/Home.jsx
@@ -0,0 +1,17 @@
+import '../styles/Home.css';
+function Home () {
+    return ( 
+        <>
+        <div className="homeok">
+           <header className='text-white flex justify-between items-center p-5'>
+            <span>REACTO</span>
+            <button>Login</button>
+            <button>Sign in</button>
+           </header>
+        </div>
+        
+        </>
+     );
+}
+
+export default Home ;
